fb49d3c99fd8ade3d3f3da72a7a1096773c973fb
Dayvid007 <sorunkedavid18@gmail.com>
2023-10-17 00:49:40 -0700

    task0

--- 0-pri_str.c
+++ 0-pri_str.c
@@ -6,10 +6,8 @@
  */
 int _printf(const char *format, ...)
 {
-int i, lenny, char_string = 0;
-
+int char_string = 0;
 char c_str, *str;
-
 va_list list_of_char;
 if (format == NULL)
 return (-1);
@@ -17,29 +15,34 @@ va_start(list_of_char, format);
 while (*format)
 {
 if (*format != '%')
+{
 write(1, format, 1);
 char_string++;
+}
 else
 {
 format++;
 if (*format == '\0')
 break;
-else if (*format == '%')
+if (*format == '%')
+{
 write(1, format, 1);
 char_string++;
+}
 else if (*format == 'c')
+{
 c_str = va_arg(list_of_char, int);
-write(1, format, 1);
+write(1, &c_str, 1);
 char_string++;
+}
 else if (*format == 's')
-str = va_arg(list_of_char, char *);
-for (; str[i] != '\0'; i++)
-lenny++;
-write(1, format, lenny);
-char_string += lenny;
+{
+str = va_arg(list_of_char, char*);
+write(1, str, strlen(str));
+char_string += strlen(str);
+}
 }
 format++;
-}
 }
 va_end(list_of_char);
 return (char_string);
